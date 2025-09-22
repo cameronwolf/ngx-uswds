f93159dbd4dd4a28b3691afaf412d1091fddb552
Cameron Wolf <Cameron.Wolf@ibm.com>
2025-09-22 14:34:54 -0500

    Remove manager.js file

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,6 +1,6 @@
-import { addons } from '@storybook/addons';
-import customTheme from './custom-theme';
+// import { addons } from '@storybook/addons';
+// import customTheme from './custom-theme';
 
-addons.setConfig({
-  theme: customTheme,
-});
+// addons.setConfig({
+//   theme: customTheme,
+// });
